489c515472c97f1f2c12f963344a5c47d3e80614
Benjamin Bearce <bbearce@gmail.com>
2024-01-25 15:21:18 -0700

    classify times

--- analysis/time_btw_choices.py
+++ analysis/time_btw_choices.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt # BB
 # WKDIR='couchdb_results/Opthamology/RIM-ONE/compare_results_01_03_2023'
 
 # Linux Tower 1
-WKDIR="/sddata/app_or_generated_data/Image-Comparator-Analysis/raw_annotations/opthamology_rim-one/opthamology_rim-one_50_CompareList"
+WKDIR="/sddata/app_or_generated_data/Image-Comparator-Analysis/raw_annotations/opthamology_rim-one/"
 
 # Old
 # DATA_OUT="/projects/Image-Comparator-Analysis/analysis"
@@ -23,12 +23,20 @@ images = pd.read_csv(os.path.join("/sddata/app_or_generated_data/Image-Comparato
 images.app_image_id.max() # 158
 images.app_image_id.min() # 1
 
-
+# Compare
+# WKDIR=os.path.join(WKDIR,'opthamology_rim-one_50_CompareList')
 results_Lazcano = pd.read_csv(os.path.join(WKDIR, "Lazcano_compare_results_01_03_2023.csv"))
 results_Alryalat = pd.read_csv(os.path.join(WKDIR, "Alryalat_compare_results_01_03_2023.csv"))
 results_Seibold = pd.read_csv(os.path.join(WKDIR, "Seibold_compare_results_01_03_2023.csv"))
 results_Malik = pd.read_csv(os.path.join(WKDIR, "Malik_compare_results_01_03_2023.csv"))
 results_Ittoop = pd.read_csv(os.path.join(WKDIR, "Ittoop_compare_results_01_03_2023.csv"))
+
+# Classify
+# WKDIR=os.path.join(WKDIR,'opthamology_rim-one-10p-repeat_ClassifyList')
+results_Lazcano = pd.read_csv(os.path.join(WKDIR, "Lazcano_classify_results_10_25_2022.csv"))
+results_Seibold = pd.read_csv(os.path.join(WKDIR, "Seibold_classify_results_10_25_2022.csv"))
+results_Malik = pd.read_csv(os.path.join(WKDIR, "Malik_classify_results_10_25_2022.csv"))
+results_Ittoop = pd.read_csv(os.path.join(WKDIR, "Ittoop_classify_results_10_25_2022.csv"))
 
 
 
@@ -44,20 +52,45 @@ def time_bwt_choices(df):
 
 
 
+
+
+# Compare
 time_Lazcano = time_bwt_choices(results_Lazcano)
 time_Alryalat = time_bwt_choices(results_Alryalat)
 time_Seibold = time_bwt_choices(results_Seibold)
 time_Malik = time_bwt_choices(results_Malik)
 time_Ittoop = time_bwt_choices(results_Ittoop)
 
-abbr_time_Lazcano = time_Lazcano[['user', 'winner', 'image0', 'image1', 'image_name_0', 'image_name_1', 'date', 'time_diff']]
-abbr_time_Alryalat = time_Alryalat[['user', 'winner', 'image0', 'image1', 'image_name_0', 'image_name_1', 'date', 'time_diff']]
-abbr_time_Seibold = time_Seibold[['user', 'winner', 'image0', 'image1', 'image_name_0', 'image_name_1', 'date', 'time_diff']]
-abbr_time_Malik = time_Malik[['user', 'winner', 'image0', 'image1', 'image_name_0', 'image_name_1', 'date', 'time_diff']]
-abbr_time_Ittoop = time_Ittoop[['user', 'winner', 'image0', 'image1', 'image_name_0', 'image_name_1', 'date', 'time_diff']]
+compare_header = ['user', 'winner', 'image0', 'image1', 'image_name_0', 'image_name_1', 'date', 'time_diff']
+abbr_time_Lazcano = time_Lazcano[compare_header]
+abbr_time_Alryalat = time_Alryalat[compare_header]
+abbr_time_Seibold = time_Seibold[compare_header]
+abbr_time_Malik = time_Malik[compare_header]
+abbr_time_Ittoop = time_Ittoop[compare_header]
 
-abbr_time_Lazcano.to_csv(os.path.join(DATA_OUT, 'time_Lazcano.csv'), index=None)
-abbr_time_Alryalat.to_csv(os.path.join(DATA_OUT, 'time_Alryalat.csv'), index=None)
-abbr_time_Seibold.to_csv(os.path.join(DATA_OUT, 'time_Seibold.csv'), index=None)
-abbr_time_Malik.to_csv(os.path.join(DATA_OUT, 'time_Malik.csv'), index=None)
-abbr_time_Ittoop.to_csv(os.path.join(DATA_OUT, 'time_Ittoop.csv'), index=None)
+abbr_time_Lazcano.to_csv(os.path.join(DATA_OUT, 'time_compare_Lazcano.csv'), index=None)
+abbr_time_Alryalat.to_csv(os.path.join(DATA_OUT, 'time_compare_Alryalat.csv'), index=None)
+abbr_time_Seibold.to_csv(os.path.join(DATA_OUT, 'time_compare_Seibold.csv'), index=None)
+abbr_time_Malik.to_csv(os.path.join(DATA_OUT, 'time_compare_Malik.csv'), index=None)
+abbr_time_Ittoop.to_csv(os.path.join(DATA_OUT, 'time_compare_Ittoop.csv'), index=None)
+
+
+# Classify
+time_Lazcano = time_bwt_choices(results_Lazcano)
+# time_Alryalat = time_bwt_choices(results_Alryalat)
+time_Seibold = time_bwt_choices(results_Seibold)
+time_Malik = time_bwt_choices(results_Malik)
+time_Ittoop = time_bwt_choices(results_Ittoop)
+
+classify_header = ['user', 'image_name', 'diagnosis', 'date_string', 'time_diff']
+abbr_time_Lazcano = time_Lazcano[classify_header]
+# abbr_time_Alryalat = time_Alryalat[classify_header]
+abbr_time_Seibold = time_Seibold[classify_header]
+abbr_time_Malik = time_Malik[classify_header]
+abbr_time_Ittoop = time_Ittoop[classify_header]
+
+abbr_time_Lazcano.to_csv(os.path.join(DATA_OUT, 'time_classify_Lazcano.csv'), index=None)
+# abbr_time_Alryalat.to_csv(os.path.join(DATA_OUT, 'time_classify_Alryalat.csv'), index=None)
+abbr_time_Seibold.to_csv(os.path.join(DATA_OUT, 'time_classify_Seibold.csv'), index=None)
+abbr_time_Malik.to_csv(os.path.join(DATA_OUT, 'time_classify_Malik.csv'), index=None)
+abbr_time_Ittoop.to_csv(os.path.join(DATA_OUT, 'time_classify_Ittoop.csv'), index=None)
